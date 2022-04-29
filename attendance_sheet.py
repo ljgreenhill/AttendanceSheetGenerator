4a2485bbe8b0b287715393532fcf19048e171172
ljgreenhill <ljgreenhill3@gmail.com>
2022-04-29 10:08:27 -0400

    updated rows

--- attendance_sheet.py
+++ attendance_sheet.py
@@ -2,7 +2,7 @@ import pandas as pd
 import xlsxwriter
 
 day_time = "M 7:30-9:25"
-core_course = "MATH 1920"
+core_course = "MATH 2940"
 course_id = "ENGRG 1009-101 (9946)"
 facilitator_1 = ""
 facilitator_2 = ""
@@ -81,61 +81,135 @@ def get_letter(number):
 def write_dates(dates):
     current_col = 2
     for date in dates:
+        worksheet_at.set_column(current_col-1, current_col-1, 8)
         current_col = current_col + 1
-        worksheet.write(get_letter(current_col+1) + "8", date, header)
+        worksheet_at.write(get_letter(current_col+1) + "8", date, header)
+
+def write_dates_space(dates):
+    current_col = 2
+    for date in dates:
+        worksheet_class.set_column(current_col-1, current_col-1, 8)
+        current_col = current_col + 1
+        worksheet_class.write(get_letter(current_col+1) + "8", date, header)
 
 def write_rows():
-    current_row = 10
-    while current_row < 23:
+    current_row = 9
+    while current_row < 32:
         current_col = 0
-        worksheet.write("B" + current_row, current_col)
-        while current_col < 18:
+        if(current_row != 9):
+            worksheet_at.write("A" + str(current_row), current_row-9, border)
+        while current_col < 17:
             current_col = current_col + 1
             if(current_row%2 == 0):
-                worksheet.write(get_letter(current_col+1) + str(current_row), "TEST", light_blue_background)
+                worksheet_at.write(get_letter(current_col+1) + str(current_row), "", light_blue_background)
             else:
-                worksheet.write(get_letter(current_col+1) + str(current_row), "TEST", light_gray_background)
+                worksheet_at.write(get_letter(current_col+1) + str(current_row), "", light_gray_background)
         current_row = current_row + 1
 
 workbook = xlsxwriter.Workbook(format_workbook_name(day_time, course_id, core_course, facilitator_1, facilitator_2) + ".xlsx")
-worksheet = workbook.add_worksheet("Attendance and Grades")
+worksheet_at = workbook.add_worksheet("Attendance and Grades")
+worksheet_class = workbook.add_worksheet("Class List")
 
 bold = workbook.add_format({'bold': True})
-red = workbook.add_format({'color': 'red'})
-gray = workbook.add_format({'color': '#A5A5A5'})
+bold_border = workbook.add_format({'bold': True})
+side_border = workbook.add_format({'bold': True})
+left_border = workbook.add_format({})
+red = workbook.add_format({'color': 'red', 'bold': True})
+gray = workbook.add_format({'color': '#A5A5A5', 'bold': True})
 gray_background = workbook.add_format({'bg_color': '#A5A5A5'})
 blue_background = workbook.add_format({'bg_color': '#BDD6EE'})
 light_gray_background = workbook.add_format({'bg_color': '#ECECEC'})
 light_blue_background = workbook.add_format({'bg_color': '#DEEAF6'})
+light_gray_background.set_border()
+light_blue_background.set_border()
+bold_border.set_border()
+side_border.set_right()
+side_border.set_bottom()
+side_border.set_top()
+left_border.set_left()
+left_border.set_bottom()
+left_border.set_top()
+
+light_gray_background_bold = workbook.add_format({'bg_color': '#ECECEC', 'bold': True})
+light_gray_background_red = workbook.add_format({'bg_color': '#ECECEC', 'bold': True, 'color': 'red'})
+light_gray_background_gray = workbook.add_format({'bg_color': '#ECECEC', 'bold': True, 'color': '#A5A5A5'})
 
 title = workbook.add_format({'bg_color': '#A5A5A5', 'bold': True})
-header = workbook.add_format({'bg_color': '#BDD6EE', 'bold': True, 'align': 'center', 'text_wrap': True})
+header = workbook.add_format({'bg_color': '#BDD6EE', 'bold': True, 'align': 'center', 'text_wrap': True, 'size': 9})
+header.set_border()
+red.set_border()
+gray.set_border()
+light_gray_background_red.set_border()
+light_gray_background_gray.set_border()
 
-worksheet.write('B1', 'Fall 2022 Academic Excellence Workshop Attendance & Grade Sheet', title)
-worksheet.set_row(0, None, gray_background)
-worksheet.set_row(7, 36, blue_background)
+border = workbook.add_format({})
+border.set_border()
 
-worksheet.write('B3', 'AEW Class:', bold)
-worksheet.write('B4', 'Meeting day:', bold)
-worksheet.write('B5', 'Course ID:', bold)
-worksheet.write('B6', 'Facilitator 1:', bold)
-worksheet.write('B7', 'Facilitator 2:', bold)
+worksheet_at.write('B1', 'Fall 2022 Academic Excellence Workshop Attendance & Grade Sheet', title)
+worksheet_at.set_row(0, None, gray_background)
+worksheet_at.set_row(7, 36, blue_background)
 
-worksheet.write('C3', core_course, bold)
-worksheet.write('C4', format_day_time(day_time) + ";" + "Location: ", bold)
-worksheet.write('C5', course_id, bold)
-worksheet.write('C6', facilitator_1, bold)
-worksheet.write('C7', facilitator_2, bold)
+worksheet_at.write('B3', 'AEW Class:', bold)
+worksheet_at.write('B4', 'Meeting day:', bold)
+worksheet_at.write('B5', 'Course ID:', bold)
+worksheet_at.write('B6', 'Facilitator 1:', bold)
+worksheet_at.write('B7', 'Facilitator 2:', bold)
 
-worksheet.write('B8', "Enrolled Students (Alphabetize by Last Name)", header)
-worksheet.write('C8', "Learning Contract", header)
+worksheet_at.write('C3', core_course, bold)
+worksheet_at.write('C4', format_day_time(day_time) + ";" + "Location: ", bold)
+worksheet_at.write('C5', course_id, bold)
+worksheet_at.write('C6', facilitator_1, bold)
+worksheet_at.write('C7', facilitator_2, bold)
+
+worksheet_at.write('O3:Q3', 'Attendance Symbols*   ', bold_border)
+worksheet_at.write('O4', 'A', red)
+worksheet_at.write('O5', 'T', gray)
+worksheet_at.write('O6', 'X', border)
+
+worksheet_at.write('P4:Q4', 'Absent    ', left_border)
+worksheet_at.write('P5:Q5', 'Tardy    ', left_border)
+worksheet_at.write('P6:Q6', 'Present    ', left_border)
+
+worksheet_at.write('A8', "", header)
+worksheet_at.write('B8', "Enrolled Students (Alphabetize by Last Name)", header)
+worksheet_at.write('C8', "Learning Contract", header)
+
+worksheet_at.write('B32:E32', 'I verify that these grades are complete and accurate. ', bold)
+worksheet_at.write('O32:Q32', '*See Attendance Policies Sheet for details', bold)
+worksheet_at.write('I33', 'Signature')
+worksheet_at.write('L33', 'Date')
+worksheet_at.write('F32:I32', '__________________________________________________', bold)
+worksheet_at.write('K32:L32', '_________________________', bold)
+
+worksheet_at.write('B34:E34', 'I verify that these grades are complete and accurate. ', bold)
+worksheet_at.write('I35', 'Signature')
+worksheet_at.write('L35', 'Date')
+worksheet_at.write('F34:I34', '')
+worksheet_at.write('K34:L34', '')
+worksheet_at.write('F34:I34', '__________________________________________________', bold)
+worksheet_at.write('K34:L34', '_________________________', bold)
 
 dates = get_dates(start_date, end_date, holidays)
 write_dates(dates)
 
-worksheet.set_column(1, 1, 18)
-worksheet.set_row(7, 50)
-worksheet.set_column(3, 1+len(dates), 12)
+worksheet_at.set_column(1, 1, 22)
+worksheet_at.set_row(7, 50)
+worksheet_at.set_row(31, 60)
+worksheet_at.set_row(33, 60)
+worksheet_at.set_row(1, 3)
+
+write_rows()
+
+worksheet_at.write('A9', "", light_gray_background)
+worksheet_at.write('B9', 'Students, Ima (example)', light_gray_background_bold)
+worksheet_at.write('D9', 'A', light_gray_background_red)
+worksheet_at.write('E9', 'T', light_gray_background_gray)
+worksheet_at.write('F9', 'X', light_gray_background)
+worksheet_at.write('P3', "", side_border)
+worksheet_at.write('Q3', "", side_border)
+worksheet_at.write('Q4', "", side_border)
+worksheet_at.write('Q5', "", side_border)
+worksheet_at.write('Q6', "", side_border)
 
 workbook.close()
 
